e458fb4fa44e6f2302a052a94b8828d3d2582bbe
Deepak <deepakawstests@gmail.com>
2025-06-22 19:21:30 +0530

    Kafka correction

--- stream_data_generator/src/main/scala/Main.scala
+++ stream_data_generator/src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 import ulities.ConfigLoader
 object Main extends App{
-  println("Performing main operation : New Version and latest work sets")
+  println("Performing main operation : New Version and latest work New result in kafka")
   val configLoad = ConfigLoader.getConfig
 
 }
