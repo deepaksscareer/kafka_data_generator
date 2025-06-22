26d6313b893e92f3baa097f7d9602cd54422322b
Deepak <deepakawstests@gmail.com>
2025-06-22 19:17:41 +0530

    New corrn

--- stream_data_generator/src/main/scala/Main.scala
+++ stream_data_generator/src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 import ulities.ConfigLoader
 object Main extends App{
-  println("Performing main operation : New Version and latest")
+  println("Performing main operation : New Version and latest work sets")
   val configLoad = ConfigLoader.getConfig
 
 }
