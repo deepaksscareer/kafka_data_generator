aba41ca7dbdc9e1960174ba4479b9451a5bc81fc
Deepak <deepakawstests@gmail.com>
2025-06-22 19:14:35 +0530

    Rollback changes

--- stream_data_generator/src/main/scala/Main.scala
+++ stream_data_generator/src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 import ulities.ConfigLoader
 object Main extends App{
-  println("Performing main operation")
+  println("Performing main operation : New Version and latest")
   val configLoad = ConfigLoader.getConfig
 
 }
