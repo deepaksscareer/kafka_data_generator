454da1e4970161a959f968ecf7c40dc8d390e61d
Deepak <deepakawstests@gmail.com>
2025-06-22 19:22:26 +0530

    New corrn

--- stream_data_generator/src/main/scala/Main.scala
+++ stream_data_generator/src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 import ulities.ConfigLoader
 object Main extends App{
-  println("Performing main operation : New Version and latest work New result in kafka")
+  println("Performing main operation : New Versions")
   val configLoad = ConfigLoader.getConfig
 
 }
